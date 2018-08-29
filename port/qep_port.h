1fdb99c9790304790605a9f40e729c5e916feb83
svetad <svd@ciklum.com>
2018-08-29 15:34:26 +0300

    Compile Blinky demo with dummy QP port

--- port/qep_port.h
+++
@@ -1,8 +0,0 @@
-#ifndef __mbed_qep_port_h
-#define __mbed_qep_port_h
-
-#include <stdint.h>
-
-#include "qep.h"
-
-#endif
--- port/qs_port.h
+++
@@ -1,7 +0,0 @@
-#ifndef __mbed_qs_port_h
-#define __mbed_qs_port_h
-
-#include "qf_port.h" // use QS with QF
-#include "qs.h"      // QS platform-independent public interface
-
-#endif
