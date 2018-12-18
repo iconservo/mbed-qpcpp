da4aa3bc8d41dff2022c2b76995791718dbd7489
Yevhenii Karpliuk <yeka@ciklum.com>
2018-12-18 14:20:27 +0200

    Remove examples

--- examples/dpp/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mbed compile -t GCC_ARM -m NRF52_DK --source=../..
--- examples/dpp/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-JLinkExe -device NRF52 -if SWD -speed 4000 -jtagconf -1,-1 -autoconnect 1 -CommanderScript download_NRF52_DK.jlink
